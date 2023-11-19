f943d642421aee86276349ec95d4691592d80264
Michelle <michelleh951004@hotmail.com>
2023-11-20 02:41:55 +0900

    added scrolltotop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React, { useEffect } from 'react'
 import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import News from './pages/News'
@@ -7,10 +8,21 @@ import NewsDetails from './pages/NewsDetails'
 import NotFound from './pages/NotFound'
 import { ArticlesProvider } from './components/contexts/ArticlesContext'
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <ArticlesProvider>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/scss/index.scss'
-import { ArticlesProvider } from './components/contexts/ArticlesContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ArticlesProvider>
       <App />
-    </ArticlesProvider>
   </React.StrictMode>,
 )
